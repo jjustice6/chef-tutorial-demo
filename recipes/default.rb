ff37682803f5eea773d40c9f2a7bd23c6ce33b43
Joshua Justice <jjustice6@bloomberg.net>
2016-08-08 17:16:19 -0400

    Final tuneup for the demo

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,10 @@
 # Default recipe for the chef cookbook
 
 # A file with a simple amount of content
-#file '/etc/hello' do
-#  action :create
-#  content 'Hello world'
-#end
+file '/etc/hello' do
+  action :create
+  content 'Hello world'
+end
 
 # A file with variable content
 #file '/etc/hello' do
@@ -19,24 +19,24 @@
 #end
 
 # A long file with variable content
-template '/etc/hello' do
-  action :create
-  source 'hello.erb'
-  variables ({:hello_var => node['demo']['greeting'], 
-              :magic_word => 'Abracadabra'}) # This is a ruby hash.
-end
+#template '/etc/hello' do
+#  action :create
+#  source 'hello.erb'
+#  variables ({:hello_var => node['demo']['greeting'], 
+#              :magic_word => 'Abracadabra'}) # This is a ruby hash.
+#end
 
 # Install a package with a different name based on the OS, using standard package management
-if ubuntu?
-  package 'emacs24-nox'
-elsif centos?
-  package 'emacs-nox'
-else
-  # no package for you
-end
+#if ubuntu?
+#  package 'emacs24-nox'
+#elsif centos?
+#  package 'emacs-nox'
+#else
+#  # no package for you
+#end
 
 # Set up a user!
-user 'chef_user' do
-  password 'PlaintextPassword' # please don't do this
-  shell '/bin/bash'
-end
+#user 'chef_user' do
+#  password 'PlaintextPassword' # please don't do this
+#  shell '/bin/bash'
+#end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 describe file('/etc/hello') do
   it { should be_file }
   it { should contain 'Hello' }
